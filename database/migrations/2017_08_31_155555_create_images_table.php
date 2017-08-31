3ce7b17016dc5b226e0c47d43b0008a0f3359725
javel20 <javier_el_balla@hotmail.com>
2017-08-31 11:05:49 -0500

    migraciones

--- database/migrations/2017_08_31_155555_create_images_table.php
+++ database/migrations/2017_08_31_155555_create_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',150);
-            $table->string('email',150)->unique();
-            $table->string('password',200);
-            $table->enum('type',['member','admin'])->default('member');
-            $table->rememberToken();
+            $table->string('name');
+            $table->integer('article_id')->unsigned();
+
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('images');
     }
 }

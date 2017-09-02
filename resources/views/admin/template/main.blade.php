962ef1b959685577005aa645fe79dfa17ec0b075
javel20 <javier_el_balla@hotmail.com>
2017-09-02 18:28:16 -0500

    plantilla admin

---
+++ resources/views/admin/template/main.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+    <meta charset="UTF-8">
+    <title>@yield('title','default')| Panel de Administracion</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css')}}">
+
+</head>
+
+<body>
+    
+    @include('admin.template.partials.nav')
+
+    <section>
+    
+        @yield('content')
+    
+    </section>
+
+    <footer>
+    
+    </footer>
+
+    <script src="{{ asset('jquery/jquery.js') }}"></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.js') }}"></script>
+
+</body>
+
+</html>
